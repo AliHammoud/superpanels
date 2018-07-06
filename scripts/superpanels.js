3f86d95cbda15e9e5cffa313ed04dcb1ea75cb8b
Ali Hammoud <ali.hammoud.lb@gmail.com>
2018-07-06 15:42:13 +0300

    enables popup cascading

--- scripts/superpanels.js
+++ scripts/superpanels.js
@@ -1,13 +1,43 @@
-function hide(){
+var panels;
 
-  var panel = document.getElementsByClassName('superpanel')[0];
-  panel.classList.add('hidden');
+window.onload = function(e){
+  
+  panels = document.querySelectorAll('.superpanel');
+  
+  show_btns = document.querySelectorAll('.show');
+  hide_btns = document.querySelectorAll('.hide');
+  
+  for (i = 0; i < panels.length; i ++){
+    console.log(panels[i])
+  }
+  
+  for (i = 0; i < show_btns.length; i ++){
+    show_btns[i].addEventListener('click', show);
+    console.log(show_btns[i]);
+  }
+  
+  for (i = 0; i < hide_btns.length; i ++){
+    hide_btns[i].addEventListener('click', hide);
+    console.log(hide_btns[i]);
+  }
+
   
 }
 
-function show(){
+function show(e){
+  console.log('### show');
+  for(i = 0; i < panels.length; i++){
+    if (panels[i].contains(this)){
+      panels[i + 1].classList.remove('hidden');
+    } else {
+      panels[0].classList.remove('hidden');
+    }
+  } 
+}
 
-  var panel = document.getElementsByClassName('superpanel')[0];
-  panel.classList.remove('hidden');
-
+function hide(e){
+  console.log('## hide');
+  for(i = 0; i < panels.length; i++){
+    if (panels[i].contains(this)) panels[i].classList.add('hidden');
+  } 
 }
