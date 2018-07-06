c1f5b19dc5ce5ef55433f4599fdd8e144ea6fcc5
Ali Hammoud <ali.hammoud.lb@gmail.com>
2018-07-06 04:07:31 +0300

    adds first popup

---
+++ scripts/superpanels.js
@@ -0,0 +1,13 @@
+function hide(){
+
+  var panel = document.getElementsByClassName('superpanel')[0];
+  panel.classList.add('hidden');
+  
+}
+
+function show(){
+
+  var panel = document.getElementsByClassName('superpanel')[0];
+  panel.classList.remove('hidden');
+
+}
