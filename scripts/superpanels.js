1481a9ce9479a826d0c7732396bca382953df0d3
Ali Hammoud <ali.hammoud.lb@gmail.com>
2018-07-07 19:38:44 +0300

    makes popups stack side by side

--- scripts/superpanels.js
+++ scripts/superpanels.js
@@ -1,4 +1,5 @@
 var panels;
+var offsets = [];
 
 window.onload = function(e){
   
@@ -7,37 +8,74 @@ window.onload = function(e){
   show_btns = document.querySelectorAll('.show');
   hide_btns = document.querySelectorAll('.hide');
   
-  for (i = 0; i < panels.length; i ++){
-    console.log(panels[i])
-  }
-  
   for (i = 0; i < show_btns.length; i ++){
     show_btns[i].addEventListener('click', show);
-    console.log(show_btns[i]);
   }
   
   for (i = 0; i < hide_btns.length; i ++){
     hide_btns[i].addEventListener('click', hide);
-    console.log(hide_btns[i]);
   }
-
-  
 }
 
 function show(e){
-  console.log('### show');
   for(i = 0; i < panels.length; i++){
     if (panels[i].contains(this)){
-      panels[i + 1].classList.remove('hidden');
+      toggleShow(panels[i + 1]);
+      shiftLeft(panels[i], 10, i);
     } else {
-      panels[0].classList.remove('hidden');
+      toggleShow(panels[0]);
+    }
+  }
+}
+
+function hide(e){
+  for(i = 0; i < panels.length; i++){
+    if (panels[i].contains(this)){
+      for (j = i; j < panels.length; j++){
+        toggleHide(panels[j]);
+        if (j > 0){
+          shiftright(panels[j-1]);
+        }
+      }
     }
   } 
 }
 
-function hide(e){
-  console.log('## hide');
-  for(i = 0; i < panels.length; i++){
-    if (panels[i].contains(this)) panels[i].classList.add('hidden');
-  } 
+function toggleHide(node){
+  node.classList.add('hidden');
+}
+
+function toggleShow(node){
+  node.classList.remove('hidden');
+}
+
+function shiftLeft(obj, spacing, index){
+  var w = obj.getBoundingClientRect().width;
+
+  var offset = (w * (index + 1)) + w/2 + (spacing * (index + 1));
+  //var offset = (w) + w/2 + (spacing * (index + 1));
+  offsets.push(offset);
+  
+  console.log(offset);
+  console.log(offsets);
+  obj.style.transform = "translateX(-" + offset + "px) rotate(0deg)";
+  
+  /*
+  if (index == 0){
+    var offset = (1.5 * w) + spacing + "px";
+    panels[index].style.transform = "translateX(-" + offset + ") rotate(0deg)";
+  }else{
+    var offset = ((index + 1) * w) + (w/2) + spacing * (index + 1);
+    for (i = 0; i <= index; i++){
+      console.log(offset * i);
+      panels[i].style.transform = "translateX(-" + offset + "px) rotateZ(0deg)";
+    }
+  }
+  */
+}
+
+function shiftright(obj){
+  var w = obj.getBoundingClientRect().width;
+  var offset = -50 + "%";
+  obj.style.transform = "translateX(" + offset + ") rotate(0deg)";
 }
