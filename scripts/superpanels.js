f904772c8451e37b7e665a689c870abdc90f4273
Ali Hammoud <ali.hammoud.lb@gmail.com>
2018-07-09 20:41:25 +0300

    adds slight optimizations

--- scripts/superpanels.js
+++ scripts/superpanels.js
@@ -1,5 +1,4 @@
 var panels;
-var offsets = [];
 
 window.onload = function(e){
   
@@ -9,26 +8,22 @@ window.onload = function(e){
   hide_btns = document.querySelectorAll('.hide');
   hideall_btns = document.querySelectorAll('.hideall');
   
-  for (i = 0; i < show_btns.length; i ++){
+  for (var i = 0; i < show_btns.length; i ++){
     show_btns[i].addEventListener('click', show);
   }
   
-  for (i = 0; i < hide_btns.length; i ++){
+  for (var i = 0; i < hide_btns.length; i ++){
     hide_btns[i].addEventListener('click', hide);
   }
   
-  for (i = 0; i < hideall_btns.length; i ++){
+  for (var i = 0; i < hideall_btns.length; i ++){
     hideall_btns[i].addEventListener('click', hideAll);
   }
-  
-  var im = document.getElementById('im');
-  im.style.width = im.parentElement.style.width;
-  im.style.height = im.style.width;
   
   var ctx = document.getElementById('myChart');
   ctx.style.width = ctx.parentElement.style.width;
   ctx.style.height = 300 + "px";
-  
+
   var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -47,25 +42,22 @@ window.onload = function(e){
 }
 
 function show(e){
-  for(i = 0; i < panels.length; i++){
+  for (var i = 0; i < panels.length; i++){
     if (panels[i].contains(this)){
-      toggleShow(panels[i + 1]);
+      toggleShow(panels[i + 1], i + 1);
       stackIn(panels[i], 10, i);
     } else {
       toggleShow(panels[0]);
+      toggleSection('visible');
     }
   }
 }
 
 function hide(e){
-  for(i = 0; i < panels.length; i++){
+  for (var i = 0; i < panels.length; i++){
     if (panels[i].contains(this)){
-      for (j = i; j < panels.length; j++){
-        toggleHide(panels[j]);
-        if (j > 0){
-          stackOut(panels[j-1], 10, j);
-        }
-      }
+      toggleHide(panels[i]);
+      stackOut(panels[i], 10, i);
     }
   }
 }
@@ -78,7 +70,7 @@ function toggleHide(node){
   node.classList.add('hidden');
 }
 
-function toggleShow(node){
+function toggleShow(node, index){
   node.classList.remove('hidden');
 }
 
@@ -87,8 +79,7 @@ function queueIn(obj, spacing, index){
   var w = obj.getBoundingClientRect().width;
   
   var offset = (window.innerWidth / 2) - ((w + spacing) * (index + 1));
-  
-  for(i = 0; i <= index; i++){
+  for (var i = 0; i <= index; i++){
     panels[i].style.left = offset + ((w + spacing) * (i)) + "px";
   }
 }
@@ -100,33 +91,46 @@ function queueOut(obj, spacing, index){
 
   var offset = (window.innerWidth / 2) - ((w + spacing) * (index + 1));
 
-  for(i = 0; i <= index; i++){
+  for (var i = 0; i <= index; i++){
     panels[i].style.left = offset + ((w + spacing) * (i)) + "px";
   }
 }
 
 function stackIn(obj, spacing, index){
   var w = obj.getBoundingClientRect().width;
-
-  //var offset = (w * (index + 1)) + w/2 + (spacing * (index + 1));
-  var offset = window.innerHeight/1.75 + (spacing * (index + 1));
   
-  obj.style.transform = "translate(-50%, " + offset + "px) rotate(0deg)";
+  var offset = window.innerHeight - (w/4 - (spacing * (index + 1)));
+  
+  panels[index].style.top = offset + "px";
+  panels[index].style.transform = "translate(-50%, 0) rotate(0deg)";
+
   obj.classList.add("stack");
 }
 
-function stackOut(obj){
+function stackOut(obj, spacing, index){
   var w = obj.getBoundingClientRect().width;
-  var offset = -50 + "%";
-  obj.style.transform = "translateX(" + offset + ") rotate(0deg)";
+  
+  obj.style.top = "50%";
+  obj.style.transform = "translate(-50%, -50%) rotate(0deg)";
+
   obj.classList.remove("stack");
+  
+  if (index > 0){
+    panels[index - 1].style.top = "50%";
+    panels[index - 1].style.transform = "translate(-50%, -50%) rotate(0deg)";
+  }
+  
 }
 
 function stackOutAll(){
-  var offset = -50 + "%";
-  for (i = 0; i < panels.length; i++){
-    panels[i].style.transform = "translateX(" + offset + ") rotate(0deg)";
-    panels[i].classList.remove("stack");
-    panels[i].classList.add("hidden");
+  for (var i = 0; i < panels.length; i++){
+    panels[i].classList.add('hidden');
+    stackOut(panels[i], 10, i);
+    toggleSection('hidden');
   }
+}
+
+function toggleSection (state){
+  var section = document.querySelector('.supersection');
+  section.style.visibility = state;
 }
